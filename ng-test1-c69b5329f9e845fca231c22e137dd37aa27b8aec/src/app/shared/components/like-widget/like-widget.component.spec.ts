0a3fddd45b91b0b05761a12c23a18f9da166e3df
Robson <rpresotti6@gmail.com>
2021-10-03 21:36:39 -0300

    Capitulo 4

--- ng-test1-c69b5329f9e845fca231c22e137dd37aa27b8aec/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ ng-test1-c69b5329f9e845fca231c22e137dd37aa27b8aec/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LikeWidgetComponent } from './like-widget.component';
+import {LikeWidgetComponent} from './like-widget.component';
+import {UniqueIdService} from '../../services/unique-id/unique-id.service';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {LikeWidgetModule} from './like-widget.module';
 
-describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+// describe(LikeWidgetComponent.name, () => {
+//   let component: LikeWidgetComponent = null;
+//   let fixture: ComponentFixture<LikeWidgetComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ LikeWidgetComponent ]
+//     })
+//     .compileComponents();
+//   });
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(LikeWidgetComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+describe(LikeWidgetComponent.name, () => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
-    })
-    .compileComponents();
-  });
+      // declarations: [ LikeWidgetComponent],
+      // imports: [ FontAwesomeModule],
+      // providers: [UniqueIdService]
+      imports: [LikeWidgetModule]
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  // it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`, () => {
+  //   fixture.detectChanges();
+  //   component.liked.subscribe((done) => {
+  //     expect(true).toBeTruthy();
+  //     done();
+  //   });
+  //   component.like();
+  // });
+
+  it(`#${LikeWidgetComponent.prototype.like.name} should trigger (@Output liked) when called`, () => {
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
+  });
+
 });
